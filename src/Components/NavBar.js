36a85c9559d661e94f648792b64330b006367c99
saja-massimi <massimisaja@gmail.com>
2024-06-24 13:24:36 +0300

    r

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+function navBar() {
+
+    return (<>
+    <h2>test</h2>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          
+                <ul>
+                    <li>Home</li>
+                    <li>كل السور</li>
+
+                </ul>
+
+        </nav> 
+    
+    </>);
+}
+export default navBar;
